708fe3976e3904b8d4fb86617210f0a4ac39ce42
Roman Zeno <zeno385@gmail.com>
2022-01-31 18:35:06 +0200

    render FriendList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // import "./App.css";
 // import ReactDOM from "react-dom";
 import Profile from "./components/Profile";
+import Statistic from "./components/Statistics";
+import FriendList from "./components/FriendListItem";
 import user from "./data/user.json";
+import data from "./data/data.json";
+import friends from "./data/friends.json";
 
 function App() {
   return (
@@ -13,6 +17,8 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistic title={data.title} stats={data.stats} />
+      <FriendList friends={friends} />;
     </div>
   );
 }
---
+++ src/components/FriendListItem.jsx
@@ -0,0 +1,9 @@
+<ul class="friend-list">
+  const FriendList = ( friends ) =>
+  <li class="item">
+    <span class="status"></span>
+    <img class="avatar" src="" alt="User avatar" width="48" />
+    <p class="name"></p>
+  </li>
+</ul>;
+export default FriendList;
