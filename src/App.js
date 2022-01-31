52871f85f40262762f522b8c816475129e1c674e
Roman Zeno <zeno385@gmail.com>
2022-01-31 17:39:46 +0200

    render profile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 // import "./App.css";
+// import ReactDOM from "react-dom";
+import Profile from "./components/Profile";
+import user from "./data/user.json";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
 }
 
 export default App;
